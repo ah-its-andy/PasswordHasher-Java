deca7236de9151d92fc683ea3aea6bdb5e140b36
Andrew <mykge@outlook.com>
2019-01-28 20:48:56 +0800

    add compatibility tests

--- security-passwordhasher-core/src/test/java/PasswordHasherTest/PasswordHasherTests.java
+++ security-passwordhasher-core/src/test/java/PasswordHasherTest/PasswordHasherTests.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PasswordHasherTests {
+    private final static String PBKDF2_COMPATIBILITY = "AQAAAAEAACcQAAAAELJDOTDL/u80qJGRkZbmncbo/pEM+f9BRFSikvHvUZPem/GlJ5E8J4fbih3yoU+54w==";
+    private final static String AES256_COMPATIBALITY = "AgAAAAIAAAABbUaexCPOHjeIFtOw8DNefLq7BQR6gHSodyfCCyzTARpKJfXEJRMnev7NnVnO5ipHLKUr2cegnQ0bzlEUrRXksVS5O/vYhJix5YQ5FLTt85wflKD08NfC/Z8kCC52xtMz";
     private final static String TEST_PASSWORD = "TestPassword1234567890!@#$%^*&(()(__)";
     private final static List<PasswordFormatHasher> passwordFormatHashers;
     private final static BinaryConverter binaryConverter = new Base64BinaryConverter();
@@ -34,11 +36,26 @@ public class PasswordHasherTests {
     }
 
     @Test
+    public void Pbkdf2WithBase64Compatibility(){
+        PasswordHasher passwordHasher = new DefaultPasswordHasher(binaryConverter, secureRandomGenerator, passwordFormatHashers);
+        boolean flag = passwordHasher.verifyHashedPassword(PBKDF2_COMPATIBILITY, TEST_PASSWORD);
+        Assertions.assertTrue(flag);
+    }
+
+    @Test
     public void Aes256WithBase64() {
         Security.addProvider(new BouncyCastleProvider());
         PasswordHasher passwordHasher = new DefaultPasswordHasher(binaryConverter, secureRandomGenerator, passwordFormatHashers);
         String hashedPassword = passwordHasher.hashPassword(TEST_PASSWORD, FormatMarkerConsts.AES256);
         boolean flag = passwordHasher.verifyHashedPassword(hashedPassword, TEST_PASSWORD);
+        Assertions.assertTrue(flag);
+    }
+
+    @Test
+    public void Aes256WithBase64Compatibility(){
+        Security.addProvider(new BouncyCastleProvider());
+        PasswordHasher passwordHasher = new DefaultPasswordHasher(binaryConverter, secureRandomGenerator, passwordFormatHashers);
+        boolean flag = passwordHasher.verifyHashedPassword(AES256_COMPATIBALITY, TEST_PASSWORD);
         Assertions.assertTrue(flag);
     }
 }
